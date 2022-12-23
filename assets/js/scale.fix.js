49d27d2835e727ceb3ef350c0c1b554ca7a6b61a
iamsherlocked1891 <zavertiaievasky@gmail.com>
2022-12-23 15:03:14 +0100

    changed images

--- assets/js/scale.fix.js
+++ assets/js/scale.fix.js
@@ -17,90 +17,151 @@ function gestureStart() {
 }
 
 
-var sliderImages = document.querySelectorAll('.slide'),
-    arrowLeft = document.querySelector('#arrow-left'),
-    arrowRight = document.querySelector('#arrow-right'),
-    current = 0;
+var currentIndex = 1;
+displaySlides(currentIndex);
 
-function reset() {
-    for (let i = 0; i < sliderImages.length; i++) {
-        sliderImages[i].style.display = 'none';
+function displaySlides(num) {
+    var x;
+    var slides = document.getElementsByClassName("imageSlides");
+    if (num > slides.length) { currentIndex = 1 }
+    if (num < 1) { currentIndex = slides.length }
+    for (x = 0; x < slides.length; x++) {
+        slides[x].style.display = "none";
     }
+    slides[currentIndex - 1].style.display = "block";
 }
 
-function init() {
-    reset();
-    sliderImages[0].style.display = 'block';
+function setSlides(num) {
+    displaySlides(currentIndex += num);
 }
+//===========================================================================
+var currentIndexgr1 = 1;
+displaySlidesgr1(currentIndexgr1);
 
-function slideLeft() {
-    reset();
-    sliderImages[current - 1].style.display = 'block';
-    current--;
-}
-
-arrowLeft.addEventListener('click', function () {
-    if (current === 0) {
-        current = sliderImages.length;
+function displaySlidesgr1(num) {
+    var x;
+    var slidesgr1 = document.getElementsByClassName("imageSlides-gr1");
+    if (num > slidesgr1.length) { currentIndexgr1 = 1 }
+    if (num < 1) { currentIndexgr1 = slidesgr1.length }
+    for (x = 0; x < slidesgr1.length; x++) {
+        slidesgr1[x].style.display = "none";
     }
-    slideLeft();
-});
-
-function slideRight() {
-    reset();
-    sliderImages[current + 1].style.display = 'block';
-    current++;
+    slidesgr1[currentIndexgr1 - 1].style.display = "block";
 }
 
-arrowRight.addEventListener('click', function () {
-    if (current === sliderImages.length - 1) {
-        current = -1;
+function setSlidesgr1(num) {
+    displaySlidesgr1(currentIndexgr1 += num);
+}
+
+//===========================================================================
+
+var currentIndexgr2 = 1;
+displaySlidesgr2(currentIndexgr2);
+
+function displaySlidesgr2(num) {
+    var x;
+    var slidesgr2 = document.getElementsByClassName("imageSlides-gr2");
+    if (num > slidesgr2.length) { currentIndexgr2 = 1 }
+    if (num < 1) { currentIndexgr2 = slidesgr2.length }
+    for (x = 0; x < slidesgr2.length; x++) {
+        slidesgr2[x].style.display = "none";
     }
-    slideRight();
-});
+    slidesgr2[currentIndexgr2 - 1].style.display = "block";
+}
 
-init();
-// ===========================================================
-var sliderImagesMob = document.querySelectorAll('.slide--mob'),
-    arrowLeftMob = document.querySelector('#arrow-left--mob'),
-    arrowRightMob = document.querySelector('#arrow-right--mob'),
-    currentMob = 0;
+function setSlidesgr2(num) {
+    displaySlidesgr2(currentIndexgr2 += num);
+}
 
-function resetMob() {
-    for (let i = 0; i < sliderImagesMob.length; i++) {
-        sliderImagesMob[i].style.display = 'none';
+//===========================================================================
+
+
+//===========================================================================
+
+var currentIndexgr3 = 1;
+displaySlidesgr3(currentIndexgr3);
+
+function displaySlidesgr3(num) {
+    var x;
+    var slidesgr3 = document.getElementsByClassName("imageSlides-gr3");
+    if (num > slidesgr3.length) { currentIndexgr3 = 1 }
+    if (num < 1) { currentIndexgr3 = slidesgr3.length }
+    for (x = 0; x < slidesgr3.length; x++) {
+        slidesgr3[x].style.display = "none";
     }
+    slidesgr3[currentIndexgr3 - 1].style.display = "block";
 }
 
-function initMob() {
-    resetMob();
-    sliderImagesMob[0].style.display = 'block';
+function setSlidesgr3(num) {
+    displaySlidesgr3(currentIndexgr3 += num);
 }
 
-function slideLeftMob() {
-    resetMob();
-    sliderImagesMob[currentMob - 1].style.display = 'block';
-    currentMob--;
-}
+//===========================================================================
 
-arrowLeftMob.addEventListener('click', function () {
-    if (currentMob === 0) {
-        currentMob = sliderImagesMob.length;
+//===========================================================================
+
+var currentIndexpv1 = 1;
+displaySlidespv1(currentIndexpv1);
+
+function displaySlidespv1(num) {
+    var x;
+    var slidespv1 = document.getElementsByClassName("imageSlides-pv1");
+    if (num > slidespv1.length) { currentIndexpv1 = 1 }
+    if (num < 1) { currentIndexpv1 = slidespv1.length }
+    for (x = 0; x < slidespv1.length; x++) {
+        slidespv1[x].style.display = "none";
     }
-    slideLeftMob();
-});
-
-function slideRightMob() {
-    resetMob();
-    sliderImagesMob[currentMob + 1].style.display = 'block';
-    currentMob++;
+    slidespv1[currentIndexpv1 - 1].style.display = "block";
 }
 
-arrowRightMob.addEventListener('click', function () {
-    if (currentMob === sliderImagesMob.length - 1) {
-        currentMob = -1;
-    }
-    slideRightMob();
-});
+function setSlidespv1(num) {
+    displaySlidespv1(currentIndexpv1 += num);
+}
 
-initMob();
+//===========================================================================
+
+
+var currentIndexpv2 = 1;
+displaySlidespv2(currentIndexpv2);
+
+function displaySlidespv2(num) {
+    var x;
+    var slidespv2 = document.getElementsByClassName("imageSlides-pv2");
+    if (num > slidespv2.length) { currentIndexpv2 = 1 }
+    if (num < 1) { currentIndexpv2 = slidespv2.length }
+    for (x = 0; x < slidespv2.length; x++) {
+        slidespv2[x].style.display = "none";
+    }
+    slidespv2[currentIndexpv2 - 1].style.display = "block";
+}
+
+function setSlidespv2(num) {
+    displaySlidespv2(currentIndexpv2 += num);
+}
+
+//===========================================================================
+
+
+//===========================================================================
+
+var currentIndexpv3 = 1;
+displaySlidespv3(currentIndexpv3);
+
+function displaySlidespv3(num) {
+    var x;
+    var slidespv3 = document.getElementsByClassName("imageSlides-pv3");
+    if (num > slidespv3.length) { currentIndexpv3 = 1 }
+    if (num < 1) { currentIndexpv3 = slidespv3.length }
+    for (x = 0; x < slidespv3.length; x++) {
+        slidespv3[x].style.display = "none";
+    }
+    slidespv3[currentIndexpv3 - 1].style.display = "block";
+}
+
+function setSlidespv3(num) {
+    displaySlidespv3(currentIndexpv3 += num);
+}
+
+//===========================================================================
+
+
